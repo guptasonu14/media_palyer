5429442803869ab3483f5f5fa5f510768be02470
guptasonu14 <sguptabudha@gmail.com>
2024-02-03 00:30:30 +0530

    add registered and debug

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -34,6 +34,10 @@ const userSchema = new Schema({
     type: String, //cloudinary url
   },
 
+  watchHistory:{
+    type: SchemaTypes.ObjectId,
+    ref: "Video"
+  },
   password:{
     type: String,
     required: [true,'Password is required']
@@ -44,41 +48,41 @@ const userSchema = new Schema({
 },{timestamps:true});
 
 userSchema.pre("save",async function (next){
-    if(!this.isModified("password")) return next();
+  if(!this.isModified("password")) return next();
+
+  this.password = bcrypt.hash(this.password,10)
+  next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+  return await bcrypt.compare(password,this.password)
+
+}
+
+userSchema.methods.generateAccessToken = function(){
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    username: this.username,
+    fullName: this.fullName,
+  },
+  process.env.ACCESS_TOKEN_SECRET,
+  {
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+  }
+  )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+  return jwt.sign({
+    _id: this._id,
     
-    this.password = bcrypt.hash(this.password,10)
-    next()
-  })
-
-  userSchema.methods.isPasswordCorrect = async function(password){
-    return await bcrypt.compare(password,this.password)
-  
-  } 
-
-  userSchema.methods.generateAccessToken = function(){
-    return jwt.sign({
-      _id: this._id,
-      email: this.email,
-      username: this.username,
-      fullName: this.fullName,
-    },
-    process.env.ACCESS_TOKEN_SECRET,
-    {
-      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
-    }
-    )
+  },
+  process.env.REFRESH_TOKEN_SECRET,
+  {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY
   }
-  
-  userSchema.methods.generateRefreshToken = function(){
-    return jwt.sign({
-      _id: this._id,
-      
-    },
-    process.env.REFRESH_TOKEN_SECRET,
-    {
-      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
-    }
-    )
-  }
+  )
+}
 
 export const User = mongoose.model("User", userSchema);
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,23 @@
 import {Router} from "express";
-
+import { registerUser } from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js"
 const router = Router()
 
-router.route("/register")
+
+router.route("/register").post(
+    //upload is middle ware 
+    upload.fields([
+        {
+            name:"avatar",
+            maxCount:1
+        },
+        {
+            nam: "coverImage",
+            maxCount:1
+        }
+    ]),
+    
+    registerUser)
+
 
 export default router;
